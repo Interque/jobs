09d16ac1793bd6248030190392e77093037408e2
Todd Metheny <todd.metheny@gmail.com>
2015-07-08 23:33:35 -0400

    tapping into jobs from the github api. just sort of playing around with it

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,5 @@ require 'httparty'
 response = HTTParty.get('https://jobs.github.com/positions.json?description=ruby')
 
 response.each do |job|
+  Listing.create!(:title => job['title'], :description => job['description'], :organization => job['company'], :city => job['location'], :email => job['how_to_apply'], :salary => 1)
 end
