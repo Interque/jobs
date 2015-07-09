fd0222fcb237f481b3529585178ee593296b19e8
Todd Metheny <todd.metheny@gmail.com>
2015-07-08 21:20:22 -0400

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'httparty'
+
+response = HTTParty.get('https://jobs.github.com/positions.json?description=ruby')
+
+response.each do |job|
+end
