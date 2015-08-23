3a9561c3fb318c5d2ab08652c598892db08272f4
Todd Metheny <todd.metheny@gmail.com>
2015-08-23 19:47:25 -0400

    increase number of listings to 15

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -13,7 +13,7 @@ class ListingsController < ApplicationController
 
     # @listings = Listing.all
     elsif params[:search].blank?
-      @listings = Listing.page(params[:page]).per_page(12).order(:created_at => :desc)
+      @listings = Listing.page(params[:page]).per_page(15).order(:created_at => :desc)
     else
       @listings = Listing.search(params[:search])
     end
