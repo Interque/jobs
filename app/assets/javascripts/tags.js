e73423dedcd4990e950ffe46940521faeab74ff5
Todd Metheny <todd.metheny@gmail.com>
2015-08-23 19:45:42 -0400

    styled the list of tags

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(document).on('ready page:load', function(){
+  $('.tag-list-map a').addClass('add-to-token');
+  // $('.tag-list-map').replace(/,/g, '');
   $(".token-input-dropdown-facebook").css("z-index","9999");
   $('#tag_form_input').tokenInput('/listings/tags.json', {
     theme: 'facebook',
