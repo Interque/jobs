db27f626bbe79d5c07ec710e91e3f6667a72414a
Todd Metheny <todd.metheny@gmail.com>
2015-08-29 11:27:42 -0400

    removed the slack integration for now

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -1,6 +1,6 @@
 class TechnologiesController < ApplicationController
   def index
-    @technologies = Technology.all.sort { |x, y| Technology.where(:name => y.name).count <=> Technology.where(:name => x.name).count }
-    @technologies = @technologies.select { |t| Technology.where(:name => t.name).count > 5 }
+    # @technologies = Technology.all.sort { |x, y| Technology.where(:name => y.name).count <=> Technology.where(:name => x.name).count }
+    # @technologies = @technologies.select { |t| Technology.where(:name => t.name).count > 5 }
   end
 end
