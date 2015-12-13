316caf465a7e985f5743ba1e1d2b989f24683431
Todd Metheny <todd.metheny@gmail.com>
2015-12-13 17:03:22 -0500

    show jobs from your state by default by ip address of get request

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,10 +8,11 @@ class ListingsController < ApplicationController
   # GET /listings
   # GET /listings.json
   def index
-    
     if params[:tag]
       @listings = Listing.tagged_with(params[:tag].downcase) #.per_page(12).order(:created_at => :desc)
       # @listings = Listing.all
+    elsif params[:all]
+      @listings = Listing.where(:active => true).page(params[:page]).per_page(20).order(:created_at => :desc)
     elsif params[:search].blank?
       if remote_ip
         g = Geocoder.search(remote_ip)
@@ -26,25 +27,24 @@ class ListingsController < ApplicationController
       end
     elsif params[:search]
       @listings = Listing.search(params[:search])
-    elsif remote_ip
-      g = Geocoder.search(remote_ip)
-      current_state = g[0].data['region_code']
-      @listings = Listing.where(:active => true, :state => current_state).paginate(:page => params[:page], :per_page => 20)
     else
       @listings = Listing.where(:active => true).per_page(20).order(:created_at => :desc)
     end
     @listing = Listing.new
 
-    unless request.remote_ip.blank?
-      g = Geocoder.search(remote_ip)
-      puts "g: #{g.inspect}"
-      puts "g.data['region_code']: #{g[0].data['region_code']}"
-      if current_user
-        puts "last_sign_in_ip: #{current_user.last_sign_in_ip}"
-        location = Location.create(ip: g[0].data['ip'], state: g[0].data['region_code'], city: g[0].data['city'], zip: g[0].data['zip_code'], country: g[0].data['country_name'], user_id: current_user.id)
-        puts "location created"
-      end
-    end
+    # unless request.remote_ip.blank?
+    #   g = Geocoder.search(remote_ip)
+    #   puts "g: #{g.inspect}"
+    #   puts "g.data['region_code']: #{g[0].data['region_code']}"
+    #   if current_user
+    #     puts "last_sign_in_ip: #{current_user.last_sign_in_ip}"
+    #     location = Location.create(ip: g[0].data['ip'], state: g[0].data['region_code'], city: g[0].data['city'], zip: g[0].data['zip_code'], country: g[0].data['country_name'], user_id: current_user.id)
+    #     puts "location created"
+    #   end
+    # end
+  end
+
+  def active_listings
   end
 
   # GET /listings/1
