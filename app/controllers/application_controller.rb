4832782e7489f6400e92a68ada5a756e8e5b31dd
Todd Metheny <todd.metheny@gmail.com>
2016-01-09 14:58:15 -0500

    setting a cookie but not saving data yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :set_utm_cookie
+
   def remote_ip
     if request.remote_ip == '127.0.0.1'
       '104.219.179.22'
     else
       request.remote_ip
+    end
+  end
+
+  def set_utm_cookie
+    if current_user.blank? && cookies[:interque_utm_tags].blank?
+      interque_source_url = request.base_url + request.original_fullpath
+      ip = remote_ip
+      utm_stuff = {utm_source: params[:utm_source], utm_campaign: params[:utm_campaign], utm_medium: params[:utm_medium], 
+          utm_content: params[:utm_content], utm_term: params[:utm_term], url: interque_source_url, ip_address: remote_ip}.to_json
+
+      cookies[:interque_utm_tags] = {
+        value: utm_stuff,
+        expires: 1.year.from_now
+      }
     end
   end
 
